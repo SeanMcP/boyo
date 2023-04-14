3864de58649d816e3b889803c65102d6293a8794
Sean McPherson <git@seanmcp.com>
2023-04-14 07:33:30 -0400

    mostly working

---
+++ activities.js
@@ -0,0 +1,15 @@
+export default [
+  // Emoji, Name, URL, Samuel Only
+  ["🧰", "McP Tools", "https://seanmcp.com/tools", true],
+  ["🐢", "Turtle Guide", "https://seanmcp.github.io/turtle-guide", false],
+  ["🐷", "Paint Pig", "https://seanmcp.github.io/paint-pig", false],
+  ["🔦", "Spotlight", "https://seanmcp.github.io/spotlight", false],
+  ["🧠", "Memory Game", "https://seanmcp.github.io/memory-game", false],
+  ["🦝", "Reading Raccoon", "https://seanmcp.github.io/reading-raccoon", false],
+//   ["🌱", "Khan Academy", "https://khanacademy.org", true],
+//   ["🐲", "Code.org", "https://studio.code.org/sections/LJKCJP", true],
+//   ["🐱", "Bongo Cat", "https://bongo.cat", false],
+  ["🌲", "Tree builder", "https://reacttraining.com/tree/", true],
+  ["🧅", "Wick Editor", "https://www.wickeditor.com/editor/", true],
+  ["🏀", "Dot dot dot", "http://lalo.li/ddd/", true],
+];
